7f46a9f64bdc54c68927ca3d46e0d1e5f6efd927
mendirl <couillard.fabien+dev@gmail.com>
2024-08-21 10:48:06 +0200

    Update dev.zsh

--- dot_oh-my-zsh/custom/dev.zsh
+++ dot_oh-my-zsh/custom/dev.zsh
@@ -1,2 +1,10 @@
-export PATH=~/.npm-global/bin:$PATH
+alias vpn=hotspotshield
+
+#export NPM_CONFIG_PREFIX=~/.npm-global
+#export PATH=~/.npm-global/bin:$PATH
 export PATH=~/bin:$PATH
+
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
